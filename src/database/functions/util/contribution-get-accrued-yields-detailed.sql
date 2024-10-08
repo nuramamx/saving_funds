cc8fbb3b6bbddd09c7bbbc0b10842516e2db7842
Christian Estrella <=>
2024-10-07 22:22:28 -0600

    Fix on saving fund yields calc.

--- src/database/functions/util/contribution-get-accrued-yields-detailed.sql
+++ src/database/functions/util/contribution-get-accrued-yields-detailed.sql
@@ -1,7 +1,7 @@
 --drop function process.contribution_get_accrued_yields_detailed;
 create or replace function process.contribution_get_accrued_yields_detailed(
   in p_saving_fund_id integer,
-  in p_year integer default null
+  in p_year integer default 0
 ) returns table (
   "year" integer,
   transaction_date text,
@@ -44,17 +44,38 @@ begin
       ,t.amount
       ,t.transaction_type
       ,sum(t.amount) over (order by t.transaction_date rows between unbounded preceding and current row) as running_balance
-      ,extract(year from t.transaction_date) as year
+      ,transaction_detail.year
     from transaction_data as t
+    left join lateral (
+      select extract(year from t.transaction_date) as year
+    ) transaction_detail on true
+    --where (p_year = 0 or transaction_detail.year = p_year)
+  ),
+  yearly_contributions as (
+    select
+      transaction_detail.year
+      ,sum(s.amount) as yearly_contributions
+    from sorted_transactions as s
+    left join lateral (
+      select extract(year from s.transaction_date) as year
+    ) transaction_detail on true
+    where s.transaction_type = 'contribution'
+    --and (p_year = 0 or transaction_detail.year = p_year)
+    group by transaction_detail.year
   ),
   yearly_yields_calculation as (
     select
       s."year"
       ,max(s.running_balance) as end_of_year_balance
       ,count(s.*) filter (where s.transaction_type = 'contribution') as contribution_count
+      ,yc.yearly_contributions
     from sorted_transactions as s
+    left join yearly_contributions yc on s.year = yc.year
     where s.transaction_type = 'contribution'
-    group by s."year"
+    --and (p_year = 0 or s.year = p_year)
+    group by
+      s."year"
+      ,yc.yearly_contributions
   ),
   yields_distribution as (
     select
@@ -69,7 +90,7 @@ begin
       ,case
         when s.transaction_type = 'contribution' and extract(year from s.transaction_date) < extract(year from current_date) then
           -- distribute the annual yields proportionally based on the number of contributions in the year
-          ((y.end_of_year_balance * annual_rate.rate) / 100) / y.contribution_count
+          ((y.yearly_contributions * annual_rate.rate) / 100) / y.contribution_count
         else
           0
       end as partial_yields
@@ -92,7 +113,7 @@ begin
       ,r.running_balance::numeric(20,6)
       ,r.partial_yields::numeric(20,6)
   from yields_distribution as r
-  where p_year is null or r."year"::integer = p_year
+  where (p_year = 0 or r."year"::integer = p_year)
   order by r.transaction_date;
 end;
 $$ language plpgsql;
